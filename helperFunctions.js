9eb9e160f44ab7405d46bee0357340bdfa762761
Qurtyslyn <qurtys.lyn@gmail.com>
2025-08-03 14:16:32 +0000

    Moved Functions to helperFunctions.js and added comments

--- helperFunctions.js
+++ helperFunctions.js
@@ -1,3 +1,4 @@
+//This function returns border, fill colors, and opacity for Polygon layers
 function getPolygonColors (eventName)
 {
     switch(eventName) {
@@ -18,6 +19,7 @@ function getPolygonColors (eventName)
     }
 }
 
+//Returns list of current legend items
 function getLegendItems()
 {
     var legend = document.getElementsByClassName("legend")[0];
@@ -34,13 +36,14 @@ function getLegendItems()
     return itemList;
 }
 
+//Adds a layer to the map legend
 function addToLegend(event)
 {
-    //'<span id="Fire"><span class="symbol fire"></span><span>Wildfire</span></span><br>';
     var legend = document.getElementsByClassName("legend")[0];
 
     var colors = getPolygonColors(event);
 
+    //Convert Hex to decimal for RGBA function
     var r = parseInt(colors.fillColor.substring(1,3), 16);
     var g = parseInt(colors.fillColor.substring(3,5), 16);
     var b = parseInt(colors.fillColor.substring(5,7), 16);
@@ -54,4 +57,52 @@ function addToLegend(event)
     var border = r.toString() + "," + g.toString() + "," + b.toString();
 
     legend.innerHTML += '<span id="' + event.replaceAll(" ","") +'"><span class="symbol square" style="background: rgba(' + background + ',0.5); border-color: rgba(' + border +',1.0)"></span><span>' + event + '</span></span><br>';
+}
+
+//Functions to create Popup texts for Alert types
+//On Each Feature function for fire layer
+function onEachFeatureFire(feature, layer)
+{
+    var date = new Date(feature.properties.attr_FireDiscoveryDateTime);
+    var updated = new Date(feature.properties.poly_DateCurrent);
+    var popupText = "Incident Name: " + feature.properties.poly_IncidentName + "<br>";
+    popupText += "Discovery Date: " + date.toLocaleString()  + "<br>";
+    popupText += "Last Updated: " + updated.toLocaleString() + "<br>";
+    popupText += "Origin: " + feature.properties.attr_InitialLatitude + ", " + feature.properties.attr_InitialLongitude + "<br>";
+    popupText += "Estimated Size: " + (Math.round(feature.properties.poly_Acres_AutoCalc * 100) / 100) + "<br>";
+    popupText += "Containment: " + feature.properties.attr_PercentContained + "%";
+    layer.bindPopup(popupText);
+}
+
+//On Each Feature function for Earthquake layer
+function onEachFeatureEQ(feature, layer)
+{
+    var date = new Date(feature.properties.time);
+    var popupText = "Incident Name: " + feature.properties.title + "<br>";
+    popupText += "Date: " + date.toLocaleString()  + "<br>";
+    popupText += "Origin: " + feature.geometry.coordinates[1] + ", " + feature.geometry.coordinates[0] + "<br>";
+    popupText += "Depth: " + feature.geometry.coordinates[2] + "km<br>";
+    popupText += "Size: M" + feature.properties.mag;
+    layer.bindPopup(popupText);
+}
+
+//On Each Feature function for NWS alert layers
+function onEachFeatureNWS(feature, layer)
+{
+    var starts = new Date(feature.properties.effective);
+    var expires = new Date(feature.properties.expires);
+    var popupText = "Alert: " + feature.properties.event + "<br>";
+    popupText += "Starts: " + starts.toLocaleString()  + "<br>";
+    popupText += "Expires: " + expires.toLocaleString()  + "<br>";
+    popupText += "Issuer: " + feature.properties.senderName + "<br>";
+    popupText += "Description: " + feature.properties.description + "<br><br>";
+    popupText += "Instructions: " + feature.properties.instruction;
+    layer.bindPopup(popupText);
+
+    //If alert does not exist in the legend, add it to the Legend
+    if(!(getLegendItems().includes(feature.properties.event.replaceAll(" ",""))))
+    {
+        //console.log(feature.properties.event);
+        addToLegend(feature.properties.event);
+    }
 }
