3e36480056d376a0aa07a1d0e27b106586ecad2f
Curtis Blackley <qurtys.lyn@gmail.com>
2026-06-04 13:29:52 -0600

    Replaced " wiht ' for ? replacement in escapeHTML

--- helperFunctions.js
+++ helperFunctions.js
@@ -70,7 +70,7 @@ function escapeHTML(str) {
             '>': '&gt;',
             "'": '&#39;',
             '"': '&quot;',
-            '?': '&quot;', //Volanoes XML data has ? in place of '
+            '?': '&#39;', //Volanoes XML data has ? in place of '
         }[tag] || tag)
     );
 }
