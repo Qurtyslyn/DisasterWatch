2b74facb235c4ab759941ee1a9397521747bccdd
Curtis Blackley <qurtys.lyn@gmail.com>
2026-06-17 12:26:25 -0600

    Updated Hurricane and Tropical Storm colors

--- helperFunctions.js
+++ helperFunctions.js
@@ -15,7 +15,9 @@ function getPolygonColors (eventName)
     case "Dust Advisory" : return {color: "#6E5937",fillColor: "#6E5937"}; break;
     case "Local Area Emergency" : return {color: "#FF0000",fillColor: "#0000FF", fillOpacity: 0.65}; break;
     case "Tornado Warning" : return {color: "#FF0000",fillColor: "#ffff00", fillOpacity: 0.65}; break;
-    case "Tornado Watcb" : return {color: "#ffff00",fillColor: "#ffff00"}; break;
+    case "Tornado Watch" : return {color: "#ffff00",fillColor: "#ffff00"}; break;
+    case "Tropical Storm Warning" : return {color: "#ffb700",fillColor: "#ffff00", fillOpacity: 0.65}; break;
+    case "Tropical Storm Watch" : return {color: "#ffb700",fillColor: "#ffb700"}; break;
     default: return {color: "#000000",fillColor: "#000000"};
 
     }
